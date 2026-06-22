bc576848b1450bb2051d3679ea9de5bca1e13c59
Dany99486 <daniel8bravo@gmail.com>
2026-06-22 20:36:52 +0000

    Added tests

---
+++ benchmarks/mathematical_model_tests/5_2_1/III/sol_k1.sol
@@ -0,0 +1,10 @@
+# Solution for model obj
+# Objective value = 10005
+TotalCost_Weighted 10000
+TotalLatency_Weighted 5
+TotalCost 20000
+TotalLatency 10
+x_w0_c0 10
+y_w0_c0 0
+x_w0_c1 0
+y_w0_c1 10
---
+++ benchmarks/mathematical_model_tests/5_2_1/III/sol_k1000.sol
@@ -0,0 +1,10 @@
+# Solution for model obj
+# Objective value = 12500
+TotalCost_Weighted 12500
+TotalLatency_Weighted 0
+TotalCost 25000
+TotalLatency 0
+x_w0_c0 0
+y_w0_c0 0
+x_w0_c1 10
+y_w0_c1 0
